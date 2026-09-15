079ab2343081b44e1a87510d3438b7b80850081d
Ardika <ardikahidayaturrohman@gmail.com>
2026-09-15 09:31:21 +0700

    added execution time with Once per request filter

---
+++ src/main/java/com/dproject/daniel_book_store/config/RequestPerformenceFilter.java
@@ -0,0 +1,44 @@
+package com.dproject.daniel_book_store.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class RequestPerformenceFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        StopWatch sw = new StopWatch();
+        sw.start();
+
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+
+        try {
+            filterChain.doFilter(request, response);
+        }finally {
+            sw.stop();
+            long excutionTime = sw.getTotalTimeMillis();
+
+            response.setHeader("X-response-time: ", excutionTime + "ms");
+
+            int status = response.getStatus();
+            if(status >= 500){
+                log.error("HTTP: {} {} | Status: {} | Execution Time: {} ms", method, uri, status, excutionTime);
+            } else if (status >= 400) {
+                log.warn("HTTP: {} {} | Status: {} | Execution Time: {} ms", method, uri, status, excutionTime);
+            }else {
+                log.info("HTTP: {} {} | Status: {} | Execution Time: {} ms", method, uri, status, excutionTime);
+            }
+        }
+    }
+}
--- src/main/java/com/dproject/daniel_book_store/config/WebConfig.java
+++ src/main/java/com/dproject/daniel_book_store/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.dproject.daniel_book_store.helper;
+package com.dproject.daniel_book_store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
